f4922084dedde90e35297856155f0519b72ef7e7
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-24 19:03:33 +0100

    Create blessing select screen

--- src/wdfeer/BlessingsMod.kt
+++ src/wdfeer/BlessingsMod.kt
@@ -1,8 +1,36 @@
 package wdfeer
 
+import arc.Events
+import arc.util.Time
+import mindustry.game.EventType
 import mindustry.mod.*
+import mindustry.ui.dialogs.BaseDialog
 
 @Suppress("unused")
 class BlessingsMod : Mod() {
+    init {
+        Events.on(EventType.ClientLoadEvent::class.java) {
+            Time.runTask(10f) {
+                BaseDialog("Select Blessing").apply {
+                    cont.apply {
+                        for (blessing in Blessing.entries) {
+                            table {
+                                button(blessing.name) {
+                                    // TODO
+                                    hide()
+                                }.pad(20f).size(90f, 40f)
+                                add(blessing.description)
+                            }.row()
+                        }
+                    }
+                    show()
+                }
+            }
+        }
+    }
+}
 
+enum class Blessing(val description: String) {
+    None("Unchanged mindustry experience"),
+    Reimu("Built blocks have +100% max hp"),
 }
