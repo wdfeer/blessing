e72e5ed5bcdf9e974cd198b80a7b68a7bd662e72
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-24 19:15:08 +0100

    Create more blessings

--- src/wdfeer/BlessingsMod.kt
+++ src/wdfeer/BlessingsMod.kt
@@ -5,6 +5,8 @@ import arc.util.Time
 import mindustry.game.EventType
 import mindustry.mod.*
 import mindustry.ui.dialogs.BaseDialog
+import mindustry.world.blocks.defense.turrets.ItemTurret.ItemTurretBuild
+import mindustry.world.blocks.defense.turrets.Turret.TurretBuild
 
 @Suppress("unused")
 class BlessingsMod : Mod() {
@@ -20,7 +22,7 @@ class BlessingsMod : Mod() {
                                 button(blessing.name) {
                                     activeBlessing = blessing
                                     hide()
-                                }.pad(20f).size(100f, 50f)
+                                }.pad(20f).size(130f, 60f)
                                 add(blessing.description)
                             }.row()
                         }
@@ -31,9 +33,22 @@ class BlessingsMod : Mod() {
         }
 
         Events.on(EventType.BlockBuildEndEvent::class.java) {
+            it.tile.build ?: return@on
             when (activeBlessing) {
                 Blessing.None -> {}
-                Blessing.Reimu -> it.tile.build.maxHealth *= 2
+                Blessing.Reimu -> it.tile.build.maxHealth *= 1.2f
+                Blessing.Nitori -> {
+                    if (it.tile.build is TurretBuild)
+                        it.tile.build.maxHealth *= 1.4f
+                }
+                Blessing.Takane -> {
+                    if (it.tile.build is TurretBuild)
+                        it.tile.build.maxHealth *= 2f
+                    else {
+                        it.tile.build.maxHealth *= 0.7f
+                        it.tile.build.health *= 0.7f
+                    }
+                }
             }
         }
     }
@@ -41,5 +56,7 @@ class BlessingsMod : Mod() {
 
 enum class Blessing(val description: String) {
     None("Unchanged mindustry experience"),
-    Reimu("Built blocks have +100% max hp"),
+    Reimu("Built blocks have +20% hp"),
+    Nitori("Built turrets have +40% hp"),
+    Takane("Built turrets have doubled hp, but all other blocks have -30% hp")
 }
