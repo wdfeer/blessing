9752fbeac03f34809b72308d4ae7a1b2f87dd23e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-24 19:44:40 +0100

    Create Sanae healing blessing

--- src/wdfeer/BlessingsMod.kt
+++ src/wdfeer/BlessingsMod.kt
@@ -1,16 +1,19 @@
 package wdfeer
 
+import arc.Core
 import arc.Events
 import arc.util.Time
+import mindustry.Vars
 import mindustry.game.EventType
+import mindustry.logic.LExecutor.Var
 import mindustry.mod.*
 import mindustry.ui.dialogs.BaseDialog
-import mindustry.world.blocks.defense.turrets.ItemTurret.ItemTurretBuild
 import mindustry.world.blocks.defense.turrets.Turret.TurretBuild
+import wdfeer.Blessing.*
 
 @Suppress("unused")
 class BlessingsMod : Mod() {
-    var activeBlessing = Blessing.None
+    var activeBlessing = None
 
     init {
         Events.on(EventType.ClientLoadEvent::class.java) {
@@ -35,28 +38,49 @@ class BlessingsMod : Mod() {
         Events.on(EventType.BlockBuildEndEvent::class.java) {
             it.tile.build ?: return@on
             when (activeBlessing) {
-                Blessing.None -> {}
-                Blessing.Reimu -> it.tile.build.maxHealth *= 1.2f
-                Blessing.Nitori -> {
-                    if (it.tile.build is TurretBuild)
-                        it.tile.build.maxHealth *= 1.4f
-                }
-                Blessing.Takane -> {
-                    if (it.tile.build is TurretBuild)
-                        it.tile.build.maxHealth *= 2f
+                Reimu -> it.tile.build.maxHealth *= 1.2f
+                Nitori -> if (it.tile.build is TurretBuild) it.tile.build.maxHealth *= 1.4f
+                Takane -> {
+                    if (it.tile.build is TurretBuild) it.tile.build.maxHealth *= 2f
                     else {
                         it.tile.build.maxHealth *= 0.7f
                         it.tile.build.health *= 0.7f
                     }
                 }
+
+                else -> {}
             }
+        }
+
+        scheduleUpdate()
+    }
+
+
+    private fun update() {
+        val delta = Time.delta
+        when (activeBlessing) {
+            Sanae -> Vars.player.unit().heal(40f * delta)
+            Aya -> {
+                Vars.player.unit().speedMultiplier = 3f
+            }
+
+            else -> {}
+        }
+    }
+
+    private fun scheduleUpdate() {
+        Time.runTask(1f) {
+            if (Vars.state.isGame && !Vars.state.isPaused) update()
+            scheduleUpdate()
         }
     }
 }
 
 enum class Blessing(val description: String) {
     None("Unchanged mindustry experience"),
-    Reimu("Built blocks have +20% hp"),
-    Nitori("Built turrets have +40% hp"),
-    Takane("Built turrets have doubled hp, but all other blocks have -30% hp")
+    Reimu("Built blocks have +20% hp."),
+    Nitori("Built turrets have +40% hp."),
+    Takane("Built turrets have doubled hp, but all other blocks have -30% hp."),
+    Sanae("Controlled unit heals 40 hp / second."),
+    Aya("Controlled unit travels thrice as fast.")
 }
