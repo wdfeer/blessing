39d1a3d30d94fe38ef4f59a21347b00eef36fde2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-30 18:14:57 +0100

    Create server->client state packet

--- src/wdfeer/Network.kt
+++ src/wdfeer/Network.kt
@@ -6,28 +6,63 @@ import arc.util.io.Reads
 import arc.util.io.Writes
 import mindustry.Vars
 import mindustry.game.EventType
+import mindustry.gen.Groups
+import mindustry.gen.Player
 import mindustry.net.Net
 import mindustry.net.NetConnection
 import mindustry.net.Packet
 
 fun BlessingMod.initNet() {
-    Net.registerPacket(::BlessingPacket)
-    Vars.net.handleServer(BlessingPacket::class.java) { connection: NetConnection, packet: BlessingPacket ->
+    Net.registerPacket(::SingleBlessingPacket)
+
+    Vars.net.handleServer(SingleBlessingPacket::class.java) { connection: NetConnection, packet: SingleBlessingPacket ->
         state.remote[connection.player] = packet.blessing!!
+        Vars.net.send(BlessingStatePacket(state.blessings), true)
     }
+
     Events.on(EventType.ClientServerConnectEvent::class.java) {
         Time.run(10f) {
-            Vars.net.send(BlessingPacket(state.local), true)
+            Vars.net.send(SingleBlessingPacket(state.local), true)
         }
+    }
+    Vars.net.handleClient(BlessingStatePacket::class.java) {
+        state.remote += it.blessings!!
     }
 }
 
-class BlessingPacket(var blessing: Blessing? = null) : Packet() {
+private class SingleBlessingPacket(var blessing: Blessing? = null) : Packet() {
     override fun read(read: Reads) {
         blessing = Blessing.entries[read.i()]
     }
 
     override fun write(write: Writes) {
         write.i(blessing!!.ordinal)
+    }
+}
+
+private class BlessingStatePacket(var blessings: Map<Player, Blessing>? = null) : Packet() {
+    override fun read(read: Reads) {
+        val length = read.s()
+        val pairs: MutableList<Pair<Player, Blessing>> = mutableListOf()
+        for (i in 0 until length) {
+            pairs.add(read.pair())
+        }
+        blessings = pairs.toMap()
+    }
+
+    private fun Reads.pair(): Pair<Player, Blessing> {
+        return Groups.player.find { it.id == i() } to Blessing.entries[i()]
+    }
+
+    override fun write(write: Writes) {
+        write.s(blessings!!.size)
+        for ((player, blessing) in blessings!!) {
+            write.pair(player to blessing)
+        }
+    }
+
+    private fun Writes.pair(pair: Pair<Player, Blessing>) {
+        this.i(pair.first.id)
+        this.i(pair.second.ordinal)
     }
 }
